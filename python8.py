cee5a09f0a9b15abf141b27846d669e656173037
yashasnc123 <yashasnc123@gmail.com>
2026-08-19 12:20:50 +0530

    added python8

---
+++ python8.py
@@ -0,0 +1,8 @@
+age=int(input("enter your age:"))
+licence=input("do you have licence:yes/no: ")
+if age>=18 and licence=="yes":
+    print("you can drive")
+else:
+    print("you cannot drive")
+
+    
